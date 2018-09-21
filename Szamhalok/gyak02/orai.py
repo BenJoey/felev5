f8568daa4c4946f5adbbbd30fb19866ab95f8b50
Bauer Bence <bauerbence5@gmail.com>
2018-09-21 16:19:39 +0200

    Starts clean up code

--- Szamhalok/gyak02/orai.py
+++ Szamhalok/gyak02/orai.py
@@ -2,9 +2,9 @@
 import json
 import time
 
-##################################
+#################################################################
 ###Internal functions
-##################################
+#################################################################
 def isValinDemArr(demandsArr, key, val):
 	Ret = []
 	for i in xrange(0, len(demandsArr)):
@@ -20,20 +20,12 @@ def isRouteAvailable(linksArr, Route, demand):
 				return False
 	return True
 
-def SetRouteOccup(linksArr, Route, demand):
+def ModifyRoute(linksArr, Route, demand, ModVal):
 	for i in xrange(0, len(Route)-1):
 		Curr = sorted([Route[i], Route[i+1]])
 		for j in linksArr:
 			if j["points"] == Curr:
-				j["capacity"] -= demand
-	return linksArr
-
-def FreeRoute(linksArr, Route, cap):
-	for i in xrange(0, len(Route)-1):
-		Curr = sorted([Route[i], Route[i+1]])
-		for j in linksArr:
-			if j["points"] == Curr:
-				j["capacity"] += cap
+				j["capacity"] += (ModVal * demand)
 	return linksArr
 
 def FindRoute(demandObj, linksArr, RoutesArr):
@@ -47,10 +39,10 @@ def Simulate(dataObj, demandInd):
 		demandObj = data["simulation"]["demands"][i]
 		Route = FindRoute(demandObj, dataObj["links"], dataObj["possible-circuits"])
 		if Route != "Fail":
-			dataObj["links"] = SetRouteOccup(dataObj["links"], Route, demandObj["demand"])
+			dataObj["links"] = ModifyRoute(dataObj["links"], Route, demandObj["demand"], -1)
 		dataObj["simulation"]["demands"][i]["route-taken"] = Route
 		Succ = False if Route == "Fail" else True
-		dataObj["PC"] += 1
+		#dataObj["PC"] += 1
 		PrintResult(data["simulation"]["demands"][i], Succ, u"foglalás", dataObj)
 	return dataObj
 
@@ -58,14 +50,15 @@ def CloseProcess(dataObj, demandInd):
 	for i in demandInd:
 		if data["simulation"]["demands"][i]["route-taken"] != "Fail":
 			demandObj = data["simulation"]["demands"][i]
-			FreeRoute(dataObj["links"], demandObj["route-taken"], demandObj["demand"])
+			ModifyRoute(dataObj["links"], demandObj["route-taken"], demandObj["demand"], 1)
 			dataObj["simulation"]["demands"][i]["route-taken"] = ""
-			dataObj["PC"] += 1
+			#dataObj["PC"] += 1
 			PrintResult(dataObj["simulation"]["demands"][i], True, u"felszabadítás", dataObj)
 	return dataObj
 
 def PrintResult(demandObj, isSuccesful, DemType, dataObj):
 	ToPrint = []
+	dataObj["PC"] += 1
 	ToPrint.append(str(dataObj["PC"]) + u".igény " + DemType)
 	ToPrint.append(str(demandObj["end-points"][0]) + "<->" + str(demandObj["end-points"][1]) + " st")
 	if DemType == u"foglalás":
@@ -75,9 +68,9 @@ def PrintResult(demandObj, isSuccesful, DemType, dataObj):
 	ToPrint.append(Part)
 	print ':'.join(ToPrint)
 
-##################################
+#################################################################
 ###Script starts here
-##################################
+#################################################################
 with open("cs1.json", "r") as read_file:
 	data = json.load(read_file)
 
@@ -87,26 +80,14 @@ for i in data["links"]:
 for i in data["simulation"]["demands"]:
 	i["route-taken"] = ""
 
-#print data["simulation"]["demands"][2]["start-time"]
 simDur = data["simulation"]["duration"]
 data["PC"] = 0
 for i in xrange(1, simDur+1):
 	DemInd = isValinDemArr(data["simulation"]["demands"], "start-time", i)
 	if DemInd != []:
 		data = Simulate(data, DemInd)
-		#Succ = False if Ret == "Fail" else True
-		#PrintCount += 1
-		#PrintResult(data["simulation"]["demands"][DemInd], Succ, u"foglalás", PrintCount)
 	DemInd2 = isValinDemArr(data["simulation"]["demands"], "end-time", i)
 	if DemInd2 != []:
 		data = CloseProcess(data, DemInd2)
-		#PrintCount += 1
-		#PrintResult(data["simulation"]["demands"][DemInd2], Succ, u"felszabadítás", PrintCount)
+	time.sleep(1)
 print data["links"]
-
-#if "start-time" in data["simulation"]["demands"][3]:
-#for current in data["simulation"]["demands"]:
-#time.sleep(1)
-#Test = data["possible-circuits"][3]
-#T=isRouteAvailable(data["links"], Test)
-#print ([u'A', u'S01'] == ["A", "S01"])
