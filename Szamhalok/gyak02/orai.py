b22da28b2ccfc2d00f36b572cb42b95050759d5a
Bauer Bence <bauerbence5@gmail.com>
2018-09-21 16:28:14 +0200

    Commit bc want to test stuffs

--- Szamhalok/gyak02/orai.py
+++ Szamhalok/gyak02/orai.py
@@ -26,7 +26,6 @@ def ModifyRoute(linksArr, Route, demand, ModVal):
 		for j in linksArr:
 			if j["points"] == Curr:
 				j["capacity"] += (ModVal * demand)
-	return linksArr
 
 def FindRoute(demandObj, linksArr, RoutesArr):
 	for i in RoutesArr:
@@ -39,34 +38,28 @@ def Simulate(dataObj, demandInd):
 		demandObj = data["simulation"]["demands"][i]
 		Route = FindRoute(demandObj, dataObj["links"], dataObj["possible-circuits"])
 		if Route != "Fail":
-			dataObj["links"] = ModifyRoute(dataObj["links"], Route, demandObj["demand"], -1)
+			ModifyRoute(dataObj["links"], Route, demandObj["demand"], -1)
 		dataObj["simulation"]["demands"][i]["route-taken"] = Route
 		Succ = False if Route == "Fail" else True
-		#dataObj["PC"] += 1
-		PrintResult(data["simulation"]["demands"][i], Succ, u"foglalás", dataObj)
-	return dataObj
+		PrintResult(dataObj["simulation"]["demands"][i], Succ, u"foglalás", dataObj)
 
 def CloseProcess(dataObj, demandInd):
 	for i in demandInd:
-		if data["simulation"]["demands"][i]["route-taken"] != "Fail":
+		if dataObj["simulation"]["demands"][i]["route-taken"] != "Fail":
 			demandObj = data["simulation"]["demands"][i]
 			ModifyRoute(dataObj["links"], demandObj["route-taken"], demandObj["demand"], 1)
 			dataObj["simulation"]["demands"][i]["route-taken"] = ""
-			#dataObj["PC"] += 1
 			PrintResult(dataObj["simulation"]["demands"][i], True, u"felszabadítás", dataObj)
-	return dataObj
 
 def PrintResult(demandObj, isSuccesful, DemType, dataObj):
-	ToPrint = []
 	dataObj["PC"] += 1
-	ToPrint.append(str(dataObj["PC"]) + u".igény " + DemType)
-	ToPrint.append(str(demandObj["end-points"][0]) + "<->" + str(demandObj["end-points"][1]) + " st")
+	ToPrint = str(dataObj["PC"]) + u".igény " + DemType + ":"
+	ToPrint += str(demandObj["end-points"][0]) + "<->" + str(demandObj["end-points"][1]) + " st:"
 	if DemType == u"foglalás":
-		Part = str(demandObj["start-time"]) + ("-sikeres" if isSuccesful else "-sikertelen")
+		ToPrint += str(demandObj["start-time"]) + ("-sikeres" if isSuccesful else "-sikertelen")
 	else:
-		Part = str(demandObj["end-time"])
-	ToPrint.append(Part)
-	print ':'.join(ToPrint)
+		ToPrint += str(demandObj["end-time"])
+	print ToPrint
 
 #################################################################
 ###Script starts here
@@ -82,12 +75,12 @@ for i in data["simulation"]["demands"]:
 
 simDur = data["simulation"]["duration"]
 data["PC"] = 0
-for i in xrange(1, simDur+1):
+for i in xrange(1, simDur + 1):
 	DemInd = isValinDemArr(data["simulation"]["demands"], "start-time", i)
 	if DemInd != []:
-		data = Simulate(data, DemInd)
+		Simulate(data, DemInd)
 	DemInd2 = isValinDemArr(data["simulation"]["demands"], "end-time", i)
 	if DemInd2 != []:
-		data = CloseProcess(data, DemInd2)
-	time.sleep(1)
+		CloseProcess(data, DemInd2)
+	#time.sleep(1)
 print data["links"]
