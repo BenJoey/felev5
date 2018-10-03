d3f57286a4224bc3e55be2b9349b8b0cc0cc45bd
Bauer Bence <bauerbence5@gmail.com>
2018-10-03 19:03:35 +0200

    Szamhalok|gyak04 task done

--- Szamhalok/gyak04/orai/calcClient.py
+++ Szamhalok/gyak04/orai/calcClient.py
@@ -1,13 +1,18 @@
 import socket
 import sys
+import struct
 
-server_addr = ('pc6w05', 10000)
+server_addr = ('localhost', 10000)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(server_addr)
-#client.sendall("helo szerver")
+
+values = (int(sys.argv[1]), sys.argv[2], int(sys.argv[3]))
+packer = struct.Struct('I 1s I')
+packed_data = packer.pack(*values)
+client.sendall(packed_data)
 
 data = client.recv(20)
-print "server uzeni: ", data
+print data
 
 client.close()
--- Szamhalok/gyak04/orai/calcServer.py
+++ Szamhalok/gyak04/orai/calcServer.py
@@ -1,7 +1,50 @@
 import socket
+import select
+import struct
 
 server_address = ('localhost', 10000)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(server_adress)
-
+server.bind(server_address)
 server.listen(1)
+input = [server]
+
+while input:
+    timeout = 1
+    readable, writeable, exc = select.select(input, input, input, timeout)
+    
+    if not (readable or writeable or exc):
+        print "Timeout"
+    
+    for s in readable:
+        if s is server:
+            client, client_addr = s.accept()
+            print "Uj kliens", client_addr
+            input.append(client)
+        else:
+            try:
+                data = s.recv(20)
+                if data:
+                    unpacker = struct.Struct('I 1s I')
+                    unpacked_data = unpacker.unpack(data)
+                    Op = unpacked_data[1]
+                    ToRet = str(unpacked_data[0]) + Op + str(unpacked_data[2]) + "="
+                    if Op == '+':
+                        ToRet += str(unpacked_data[0] + unpacked_data[2])
+                    if Op == '-':
+                        ToRet += str(unpacked_data[0] - unpacked_data[2])
+                    if Op == '*':
+                        ToRet += str(unpacked_data[0] * unpacked_data[2])
+                    if Op == '/':
+                        if unpacked_data[2] == 0:
+                            ToRet = "Error:Osztas nullaval"
+                        else:
+                            ToRet += str(unpacked_data[0] / unpacked_data[2])
+                    s.sendall(ToRet)
+                else:
+                    s.close()
+                    input.remove(s)
+                    print "Kliens kilepett"
+            except socket.error, msg:
+                    s.close()
+                    input.remove(s)
+                    print "Kliens hibaval kilepett"
