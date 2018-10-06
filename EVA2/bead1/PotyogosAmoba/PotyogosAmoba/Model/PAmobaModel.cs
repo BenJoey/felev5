439c010a12cbba1ad94294a1aee40779098f9f31
Bauer Bence <bauerbence5@gmail.com>
2018-10-06 09:49:40 +0200

    EVA2|Fixes gameover check

--- EVA2/bead1/PotyogosAmoba/PotyogosAmoba/Model/PAmobaModel.cs
+++ EVA2/bead1/PotyogosAmoba/PotyogosAmoba/Model/PAmobaModel.cs
@@ -169,9 +169,9 @@ namespace PotyogosAmoba.Model
             {
                 for (Int32 j = 0; j < gameSize; j++)
                 {
-                    if (GetFieldValue(i, j) != Player.NoPlayer && GetFieldValue(i, j) == GetFieldValue(i + 1, j) && GetFieldValue(i + 1, j) == GetFieldValue(i + 2, j) && GetFieldValue(i + 2, j) == GetFieldValue(i + 3, j))
+                    if (GetFieldValue(i, j) != Player.NoPlayer && GetFieldValue(i, j) == GetFieldValue(i, j + 1) && GetFieldValue(i, j + 1) == GetFieldValue(i, j + 2) && GetFieldValue(i, j + 2) == GetFieldValue(i, j + 3))
                     {
-                        Tuple<Int32, Int32>[] WinningPlace = { Tuple.Create(i, j), Tuple.Create(i + 1, j), Tuple.Create(i + 2, j), Tuple.Create(i + 3, j) };
+                        Tuple<Int32, Int32>[] WinningPlace = { Tuple.Create(i, j), Tuple.Create(i, j + 1), Tuple.Create(i, j + 2), Tuple.Create(i, j + 3) };
                         GameOver_Signal(new AmobaEvent(GetFieldValue(i, j), playerXTime, player0Time, WinningPlace));
                     }
                     if (GetFieldValue(i, j) != Player.NoPlayer && GetFieldValue(i, j) == GetFieldValue(i + 1, j + 1) && GetFieldValue(i + 2, j + 2) == GetFieldValue(i + 3, j + 3) && GetFieldValue(i, j) == GetFieldValue(i + 2, j + 2))
