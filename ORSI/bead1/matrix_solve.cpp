98d60e5bc55cec84c61f2336bad7cee11f87d364
Bauer Bence <bauerbence5@gmail.com>
2018-10-10 12:46:33 +0200

    ORSI|Start reading the input

--- ORSI/bead1/matrix_solve.cpp
+++ ORSI/bead1/matrix_solve.cpp
@@ -1,6 +1,30 @@
+#include <fstream>
 #include <vector>
+#include <future>
 
 int main()
 {
+    std::ifstream input("input.txt");
+	
+	unsigned int N;
+	
+	input >> N;
+    
+	std::vector<std::vector<std::vector<int>>> data(N);
+    for(int i = 0; i<N;i++)
+    {
+        int Size;
+        input >> Size;
+        data[i].resize(Size);
+        for(int j = 0;j<Size;j++)
+        {
+            data[i][j].resize(Size);
+            for(int k =0;k<Size;k++)
+            {
+                input >> data[i][j][k];
+            }
+        }
+    }
+    input.close();
     return 0;
 }
