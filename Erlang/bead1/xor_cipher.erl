c933ee61346d0c3c6d47661be11333b30cfb88e7
Bauer Bence <bauerbence5@gmail.com>
2018-10-29 13:44:37 +0100

    Erlang|Fix indentation

--- Erlang/bead1/xor_cipher.erl
+++ Erlang/bead1/xor_cipher.erl
@@ -27,14 +27,14 @@ charToBitString(A) -> addExtraZeros(toBitString(A)).
 %% Converting a bitstring to a character (2 pont)
 -spec bitStringToChar(BitStr :: bitString()) -> char().
 bitStringToChar(BitStr) ->
-	{Result, _} = lists:foldl(fun(Curr, {Sofar, Index}) -> 
-			NewVal = case Curr of
-				1 -> Sofar + math:pow(2, Index);
-				0 -> Sofar
-			end,
-			{NewVal, Index+1}
-			end, {0, 0}, BitStr),
-	trunc(Result).
+    {Result, _} = lists:foldl(fun(Curr, {Sofar, Index}) -> 
+                                      NewVal = case Curr of
+                                                   1 -> Sofar + math:pow(2, Index);
+                                                   0 -> Sofar
+                                               end,
+                                      {NewVal, Index+1}
+                              end, {0, 0}, BitStr),
+    trunc(Result).
 
 %%%=============================================================================
 %%% Internal functions
@@ -46,6 +46,6 @@ toBitString(N, Acc) -> toBitString(N div 2, [N rem 2 | Acc]).
 
 -spec addExtraZeros(Acc :: bitString()) -> bitString() | {error, atom()}.
 addExtraZeros(Acc) when length(Acc) > 8 ->
-	{error, invalid_character};
+    {error, invalid_character};
 addExtraZeros(Acc) when length(Acc) == 8 ->	Acc;
 addExtraZeros(Acc) -> addExtraZeros(Acc ++ [0]).
