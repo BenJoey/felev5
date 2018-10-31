984f538d661e8491209bd9df7c2805fc6045d54a
Bauer Bence <bauerbence5@gmail.com>
2018-10-31 11:19:34 +0100

    EVA2|Init bead2 Model & Persistence copied from bead1

---
+++ EVA2/bead2/PotyogosAmoba/PotyogosAmoba/Persistence/AmobaDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PotyogosAmoba.Model;
+using System.Threading.Tasks;
+
+namespace PotyogosAmoba.Persistence
+{
+    public interface IAmobaDataAccess
+    {
+        /// <summary>
+        /// Fájl betöltése.
+        /// </summary>
+        /// <param name="path">Elérési útvonal.</param>
+        /// <returns>A fájlból beolvasott játékadatok.</returns>
+        Task<Tuple<Int32, Int32, Int32, Player, Player[,]>> LoadAsync(String path);
+
+        /// <summary>
+        /// Fájl mentése.
+        /// </summary>
+        /// <param name="path">Elérési útvonal.</param>
+        /// <param name="input">A fájlba kiírandó játékadatok.</param>
+        Task SaveAsync(String path, Tuple<Int32, Int32, Int32, Player, Player[,]> input);
+    }
+}
---
+++ EVA2/bead2/PotyogosAmoba/PotyogosAmoba/Persistence/AmobaException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PotyogosAmoba.Persistence
+{
+    /// <summary>
+    /// Amőba adatelérés kivétel típusa.
+    /// </summary>
+    public class AmobaDataException : Exception
+    {
+        /// <summary>
+        /// Amőba adatelérés kivétel példányosítása.
+        /// </summary>
+        public AmobaDataException() { }
+    }
+}
