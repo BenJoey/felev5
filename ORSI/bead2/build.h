50d5b51283b6bd7ba80fee735a52ebd06071d941
Bauer Bence <bauerbence5@gmail.com>
2018-11-06 21:23:47 +0100

    ORSI|Init class for builds

---
+++ ORSI/bead2/build.h
@@ -0,0 +1,14 @@
+#ifndef BUILD_H
+#define BUILD_H
+
+#include <string>
+
+class build {
+    private:
+    std::string Level;
+    std::string Class;
+    std::string Race;
+    std::string Gender;
+};
+
+#endif //BUILD_H
--- ORSI/bead2/click_here_for_best_wow_builds.cpp
+++ ORSI/bead2/click_here_for_best_wow_builds.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 
+#include "build.h"
+
 int main()
 {
 	std::ifstream input("input.txt");
