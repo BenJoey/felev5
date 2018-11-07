d6f0bfcd7e77642ff7cb514c5873e723442c899f
Bauer Bence <bauerbence5@gmail.com>
2018-11-07 13:29:13 +0100

    ORSI|Here comes the autism

--- ORSI/bead2/build.h
+++ ORSI/bead2/build.h
@@ -2,13 +2,30 @@
 #define BUILD_H
 
 #include <string>
+#include <iostream>
 
 class build {
     private:
-    std::string Level;
-    std::string Class;
+    int Level;
     std::string Race;
+    std::string Class;
     std::string Gender;
+    public:
+    build() {}
+    build(std::string Cla, std::string Rac,int Lev, std::string Gen) : Level(Lev), Class(Cla), Race(Rac), Gender(Gen) {}
+    // void set_level(const int& Lev){
+        // Level = Lev;
+    // }
+    bool operator==(const build& rhs) const{
+        return Level == rhs.Level && Class == rhs.Class && Race == rhs.Race && Gender == rhs.Gender;
+    }
+    void print(std::ostream& stream) const{
+        stream<<Level<<";"<<Class<<";"<<Race<<";"<<Gender<<std::endl;
+    }
 };
+std::ostream& operator<<(std::ostream& stream,const build& rhs){
+    rhs.print(stream);
+    return stream;
+}
 
 #endif //BUILD_H
--- ORSI/bead2/click_here_for_best_wow_builds.cpp
+++ ORSI/bead2/click_here_for_best_wow_builds.cpp
@@ -1,4 +1,8 @@
 #include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <string>
 
 #include "build.h"
 
@@ -7,4 +11,20 @@ int main()
 	std::ifstream input("input.txt");
 	unsigned int N;
 	input >> N;
+    std::vector<build> data;
+    std::string s;
+    while(getline(input, s)){
+        //if(!getline(input, s)) break;
+        std::istringstream ss(s);
+        std::vector<std::string> Line;
+        while(getline(ss, s, ';')){
+            //std::string sec;
+            //if(!getline(ss, sec, ';')) break;
+            Line.push_back(s);
+        }
+        std::cout<<"TEST"<<std::endl;
+        build temp(Line[0], Line[1], std::stoi(Line[2]), Line[3]);
+        data.push_back(temp);
+        std::cout<<temp<<std::endl;
+    }
 }
