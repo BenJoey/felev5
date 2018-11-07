f511904e64d936e6ab08b0cf5e377e3c40d092db
Bauer Bence <bauerbence5@gmail.com>
2018-11-07 14:46:59 +0100

    ORSI|Fix data read

--- ORSI/bead2/build.h
+++ ORSI/bead2/build.h
@@ -4,26 +4,23 @@
 #include <string>
 #include <iostream>
 
-class build {
+class Build {
     private:
     int Level;
     std::string Race;
     std::string Class;
     std::string Gender;
     public:
-    build() {}
-    build(std::string Cla, std::string Rac,int Lev, std::string Gen) : Level(Lev), Class(Cla), Race(Rac), Gender(Gen) {}
-    // void set_level(const int& Lev){
-        // Level = Lev;
-    // }
-    bool operator==(const build& rhs) const{
+    Build() {}
+    Build(std::string Cla, std::string Rac,int Lev, std::string Gen) : Level(Lev), Class(Cla), Race(Rac), Gender(Gen) {}
+    bool operator==(const Build& rhs) const{
         return Level == rhs.Level && Class == rhs.Class && Race == rhs.Race && Gender == rhs.Gender;
     }
     void print(std::ostream& stream) const{
         stream<<Level<<";"<<Class<<";"<<Race<<";"<<Gender<<std::endl;
     }
 };
-std::ostream& operator<<(std::ostream& stream,const build& rhs){
+std::ostream& operator<<(std::ostream& stream,const Build& rhs){
     rhs.print(stream);
     return stream;
 }
--- ORSI/bead2/click_here_for_best_wow_builds.cpp
+++ ORSI/bead2/click_here_for_best_wow_builds.cpp
@@ -9,22 +9,18 @@
 int main()
 {
 	std::ifstream input("input.txt");
-	unsigned int N;
-	input >> N;
-    std::vector<build> data;
+    std::vector<Build> data;
     std::string s;
+    getline(input, s);
+    unsigned int N = std::stoi(s);
     while(getline(input, s)){
-        //if(!getline(input, s)) break;
         std::istringstream ss(s);
         std::vector<std::string> Line;
         while(getline(ss, s, ';')){
-            //std::string sec;
-            //if(!getline(ss, sec, ';')) break;
             Line.push_back(s);
         }
-        std::cout<<"TEST"<<std::endl;
-        build temp(Line[0], Line[1], std::stoi(Line[2]), Line[3]);
+        Build temp(Line[0], Line[1], std::stoi(Line[2]), Line[3]);
         data.push_back(temp);
-        std::cout<<temp<<std::endl;
+        std::cout<<temp;
     }
 }
