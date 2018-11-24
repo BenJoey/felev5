1a266a54b02b4215b6dfcb6701cc36ed967f4e6d
Bauer Bence <bauerbence5@gmail.com>
2018-11-24 11:35:31 +0100

    OPRE|Splits bead2 This way bead2 only includes stuffs for task and bead1 is used for generating inputs

--- OPRE/bead2/bead2.c
+++ OPRE/bead2/bead2.c
@@ -40,39 +40,7 @@ void Order_to_File(FILE* stream, const order_t* ord){
       );
 }
 
-int Get_Offer(const int request){
-  if(request % 250 == 0)return request/250;
-  return request/250+1;
-}
-
-int is_Pos_Set(const model_t* _model, int OrdNum){
-  int i;
-  for(i=0;i<_model->length;++i){
-    if(_model->full_log[i].position == OrdNum)return 1;
-  }
-  return 0;
-}
-
-void Save_With_Del_Order(const model_t* toSave, int OrdNum){
-  FILE* file = fopen(SAVE_LOC, "w");
-  fprintf(file, "%d\n", toSave->length-1);
-  int i;
-  for(i = 0; i < toSave->length; ++i) {
-    if(OrdNum != toSave->full_log[i].position)
-      Order_to_File(file, &(toSave->full_log[i]));
-  }
-  fclose(file);
-}
-
-int Get_Next_ID(const model_t* _model){
-  int i;
-  for(i=1;i<=_model->length;++i){
-    if(is_Pos_Set(_model, i) == 0)return i;
-  }
-  return (_model->length+1);
-}
-
-void save_data(const model_t* toSave, order_t* new_Ord){
+void save_data(const model_t* toSave){
   FILE* file = fopen(SAVE_LOC, "w");
   if(file == NULL) {
     return;
@@ -81,10 +49,6 @@ void save_data(const model_t* toSave, order_t* new_Ord){
     int i;
     for(i = 0; i < toSave->length; ++i) {
       Order_to_File(file, &(toSave->full_log[i]));
-    }
-    if(new_Ord!=NULL){
-      new_Ord->position=Get_Next_ID(toSave);
-      Order_to_File(file, new_Ord);
     }
     fclose(file);
   }
@@ -119,65 +83,6 @@ void load_data(model_t* toFill){
   qsort(toFill->full_log, toFill->length, sizeof(order_t), compare_ord);
 }
 
-void print_order(const order_t* ord){
-  char _time[20];
-  strftime(_time, 20, "%F;%T", localtime(&(ord->time)));
-  printf("\nSorszam: %d   ", ord->position);
-  printf("Megrendelo neve: %s   Email-cime: %s   Telefonszama: %s   Igenye: %d\nAjanlott panelek szama: %d     Megrendeles ideje: %s\n",
-      ord->name, ord->email, ord->phone, ord->request, Get_Offer(ord->request), _time);
-}
-
-void list_by_filter(const model_t* model, const char* param, const char type){
-  int i;
-  for(i=0;i<model->length;++i){
-    char req[20];
-    sprintf(req, "%d", model->full_log[i].request);
-    switch(type){
-      case '1':
-        if(strcmp(param, model->full_log[i].name)==0)
-          print_order(&(model->full_log[i]));
-        break;
-      case '2':
-        if(strcmp(param, req)==0)
-          print_order(&(model->full_log[i]));
-        break;
-    }
-  }
-}
-
-void wait_enter(){
-  printf("\nNyomjon [Enter]-t a folytatashoz.\n");
-  while(getchar()!='\n');
-  getchar();
-}
-
-void read_order(order_t* ord) {
-  printf("Nev (max 50 karakter, szokoz nelkul): ");
-  scanf("%50s", ord->name);
-  printf("Email (max 50 karakter): ");
-  scanf("%50s", ord->email);
-  printf("Telefonszam (11 szamjegy): ");
-  scanf("%11s", ord->phone);
-  printf("Igeny: ");
-  scanf("%20d", &(ord->request));
-  strncpy(ord->state, "NotStarted", 15);
-  ord->time = time(NULL);
-}
-
-int Mod_Order(model_t* FullModel, int OrdNum){
-  int Index=-1;
-  int i;
-  for(i=0;i<FullModel->length;++i){
-    if(FullModel->full_log[i].position == OrdNum) Index = i;
-  }
-  if(Index==-1)return 0;
-  printf("\nModositani kivant megrendeles jelenlegi adatai:");
-  print_order(&(FullModel->full_log[Index]));
-  printf("Uj adatok:\n");
-  read_order(&(FullModel->full_log[Index]));
-  return 1;
-}
-
 int getSameReqOrder(model_t* FullModel, int OrdInd){
   if(strcmp(FullModel->full_log[OrdInd].state, "NotStarted")!=0) return -1;
   int ReqToFind = FullModel->full_log[OrdInd].request;
@@ -192,89 +97,7 @@ void handler(int signalnum){
   printf("Signal No. :%d", signalnum);
 }
 
-int main()
-{
-  int quit = 0;
-  char selected[1];
-  char choice[1];
+int main(){
   model_t Model;
-  while(!quit){
-    printf("----Fenyes Nap Kft----\n\nElerheto funkciok:\n");
-    printf("1: Uj rendeles rogzitese\n2: Korabbi rendeles modositasa\n3: Teljes listazas\n");
-    printf("4: Listazas szurve\n5: Ajanlat kerese\n6: Masodik beadando\n0: Kilepes\n\n");
-    load_data(&Model);
-    order_t Current;
-    scanf("%s", selected);
-    switch(selected[0]){
-      case '0':
-        quit=1;
-        break;
-      case '1':
-        printf("----Uj rendeles----\n\n");
-        read_order(&Current);
-        save_data(&Model, &Current);
-        wait_enter();
-        break;
-      case '2':
-        printf("----Modositas tipusa----\n1. Torles\n2. Adatok/Igeny szerkesztese\n");
-        scanf("%s", choice);
-        if(choice[0]=='1'||choice[0]=='2'){
-          printf("Modositani kivant megrendeles sorszama: ");
-          int OrdNum;
-          scanf("%d", &OrdNum);
-          switch(choice[0]){
-            case '1':
-              if(is_Pos_Set(&Model, OrdNum)){
-                Save_With_Del_Order(&Model, OrdNum);
-              }else{
-                printf("Nincs ilyen sorszamu megrendeles\n");
-              }
-              break;
-            case '2':
-              if(Mod_Order(&Model, OrdNum)){
-                save_data(&Model, NULL);
-                printf("Megrendeles sikeresen modositva\n");
-              }else{
-                printf("Nincs ilyen sorszamu megrendeles\n");
-              }
-              break;
-          }
-        }
-        wait_enter();
-        break;
-      case '3':
-        printf("----Megrendelesek teljes listaja:----\n");
-        int i;
-        for(i=0;i<Model.length;++i){
-          print_order(&(Model.full_log[i]));
-        }
-        wait_enter();
-        break;
-      case '4':
-        printf("----Mi szerint kivan listazni?----\n1. Nev szerint\n2. Igeny szerint\n");
-        scanf("%s", choice);
-        if(choice[0]=='1'||choice[0]=='2'){
-          printf("Keresesi parameter: ");
-          char param[50];
-          scanf("%s", param);
-          list_by_filter(&Model, param, choice[0]);
-        }
-        wait_enter();
-        break;
-      case '5':
-        printf("----Ajanlott panelek szamanak lekerdezese----\n");
-        printf("Igenye: ");
-        int req;
-        scanf("%d", &req);
-        printf("\nMaganak %d darab panel lenne az optimális.\n", Get_Offer(req));
-        wait_enter();
-        break;
-      default:
-        printf("Hibas parancs");
-        wait_enter();
-        break;
-    }
-    free(Model.full_log);
-  }
   return 0;
 }
