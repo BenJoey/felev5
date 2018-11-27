7b9f564d920fb2e413018e1a6cc320df49fb104b
Bauer Bence <bauerbence5@gmail.com>
2018-11-27 16:04:04 +0100

    ORSI|Kinda works

--- ORSI/bead3/crippling_depression.cpp
+++ ORSI/bead3/crippling_depression.cpp
@@ -75,19 +75,21 @@ void DateCompare(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count,
   }
 }
 
-void EmailCheck(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count, std::string FilterLine){
-  std::vector<std::string> validmails;
-  LineToVec(FilterLine, '|', validmails);
+//Email check and job availability search are almost identical so we so we differ them by addig a variable that tells which one is used
+//value is 0 for email and 1 for job 
+void EmailAndJob(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count, std::string FilterLine, int JobOrEmail){
+  std::vector<std::string> ValidValues;
+  LineToVec(FilterLine, '|', ValidValues);
   for(int i=0;i<data_count;++i){
     Candidate curr = source.pop();
     if(curr.Valid()){
-      std::string maildomain = curr._email.substr(curr._email.find("@")+1);
-      if(std::find(validmails.begin(), validmails.end(), maildomain) == validmails.end()) curr.Disable();
+      std::string ToCheck = JobOrEmail == 0 ? curr._email.substr(curr._email.find("@")+1) : curr._job;
+      if(std::find(ValidValues.begin(), ValidValues.end(), ToCheck) == ValidValues.end()) curr.Disable();
     }
     dest.push(curr);
   }
 }
-
+/*
 void isJobAvailable(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count, std::string FilterLine){
   std::vector<std::string> jobs;
   LineToVec(FilterLine, '|', jobs);
@@ -96,7 +98,7 @@ void isJobAvailable(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_cou
     if(curr.Valid() && std::find(jobs.begin(), jobs.end(), curr._job)==jobs.end()) curr.Disable();
     dest.push(curr);
   }
-}
+}*/
 
 void SkillCheck(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count, std::string FilterLine){
   std::vector<std::string> reqSkills;
@@ -121,7 +123,7 @@ void CVformat(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count){
   }
 }
 
-//The two size checks are almost identical so we differ them by addig a variable that tells if this is for Pic or CV
+//The two size checks are also almost identical so we differ them by addig a variable that tells if this is for Pic or CV
 //value is 0 for CV & 1 for Pic
 void Size_Check(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count, int MaxSize, int PicOrCV){
   for(int i=0;i<data_count;++i){
@@ -137,7 +139,7 @@ void Picformat(Pipe<Candidate>& source, Pipe<Candidate>& dest, int data_count, s
   LineToVec(FilterLine, '|', formats);
   for(int i=0;i<data_count;++i){
     Candidate curr = source.pop();
-    if(curr.Valid() && std::find(formats.begin(), formats.end(), curr._picfname.substr(curr._picfname.size()-4)) == formats.end()) curr.Disable();
+    if(curr.Valid() && std::find(formats.begin(), formats.end(), curr._picfname.substr(curr._picfname.find_last_of("."))) == formats.end()) curr.Disable();
     dest.push(curr);
   }
 }
@@ -146,7 +148,6 @@ void FinalPipe(Pipe<Candidate>& source, int data_count){
   std::ofstream output("output.txt");
   for(int i = 0;i<data_count;++i){
     Candidate curr = source.pop();
-    std::cout << curr._email << std::endl;
     if(curr.Valid()) output << curr._email << std::endl;
   }
   output.close();
@@ -154,17 +155,28 @@ void FinalPipe(Pipe<Candidate>& source, int data_count){
 
 int main()
 {
-  std::vector<Pipe<Candidate>> pipes(4);
+  std::vector<Pipe<Candidate>> pipes(9);
   std::ifstream input("data.txt");
+  std::ifstream filters("filters.txt");
   std::string s;
   getline(input, s);
-  std::thread t1(DateCompare, std::ref(pipes[0]), std::ref(pipes[1]), 3, "22 10 07");
-  std::thread t2(EmailCheck, std::ref(pipes[1]), std::ref(pipes[2]), 3, "gmail.com|yahoo.com|hotmail.com");
-  std::thread t3(isJobAvailable, std::ref(pipes[2]), std::ref(pipes[3]), 3, "3D Animator|Technical Lead");
-  std::thread t9(FinalPipe, std::ref(pipes[3]), 3);
+  int N = std::stoi(s);
+  getline(filters, s); std::thread t1(DateCompare, std::ref(pipes[0]), std::ref(pipes[1]), N, s);
+  getline(filters, s); std::thread t2(EmailAndJob, std::ref(pipes[1]), std::ref(pipes[2]), N, s, 0);
+  getline(filters, s); std::thread t3(EmailAndJob, std::ref(pipes[2]), std::ref(pipes[3]), N, s, 1);
+  getline(filters, s); std::thread t4(SkillCheck, std::ref(pipes[3]), std::ref(pipes[4]), N, s);
+  std::thread t5(CVformat, std::ref(pipes[4]), std::ref(pipes[5]), N);
+  getline(filters, s); std::thread t6(Size_Check, std::ref(pipes[5]), std::ref(pipes[6]), N, std::stoi(s), 0);
+  getline(filters, s); std::thread t7(Picformat, std::ref(pipes[6]), std::ref(pipes[7]), N, s);
+  getline(filters, s); std::thread t8(Size_Check, std::ref(pipes[7]), std::ref(pipes[8]), N, std::stoi(s), 1);
+  std::thread t9(FinalPipe, std::ref(pipes[8]), N);
+  filters.close();
   while(getline(input, s)){
     Candidate test(s);
     pipes[0].push(test);
   }
+  input.close();
+  t1.join();t2.join();t3.join();t4.join();
+  t5.join();t6.join();t7.join();t8.join();t9.join();
   return 0;
 }
