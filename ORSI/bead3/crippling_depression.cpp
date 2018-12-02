3cbdfb450b60b9857fc9fbc2524ec2c502e0a22f
Bauer Bence <bauerbence5@gmail.com>
2018-12-02 16:34:51 +0100

    ORSI|bead3 DONE (for for real this time)

--- ORSI/bead3/crippling_depression.cpp
+++ ORSI/bead3/crippling_depression.cpp
@@ -113,38 +113,36 @@ void Picformat(Pipe<Candidate>& source, Pipe<Candidate>& dest, const int data_co
   }
 }
 
-void FinalPipe(Pipe<Candidate>& source, const int data_count){
-  std::ofstream output("output.txt");
-  for(int i = 0;i<data_count;++i){
-    Candidate curr = source.pop();
-    if(curr.Valid) output << curr._email << std::endl;
-  }
-  output.close();
-}
-
 int main()
 {
-  std::vector<Pipe<Candidate>> pipes(9);
+  std::ofstream output("output.txt");
   std::ifstream input("data.txt");
-  std::ifstream filters("filters.txt");
   std::string s;
   getline(input, s);
   int N = std::stoi(s);
-  getline(filters, s); std::thread t1(DateCompare, std::ref(pipes[0]), std::ref(pipes[1]), N, s);
-  getline(filters, s); std::thread t2(EmailAndJob, std::ref(pipes[1]), std::ref(pipes[2]), N, s, 0);
-  getline(filters, s); std::thread t3(EmailAndJob, std::ref(pipes[2]), std::ref(pipes[3]), N, s, 1);
-  getline(filters, s); std::thread t4(SkillCheck, std::ref(pipes[3]), std::ref(pipes[4]), N, s);
-  std::thread t5(CVformat, std::ref(pipes[4]), std::ref(pipes[5]), N);
-  getline(filters, s); std::thread t6(Size_Check, std::ref(pipes[5]), std::ref(pipes[6]), N, std::stoi(s), 0);
-  getline(filters, s); std::thread t7(Picformat, std::ref(pipes[6]), std::ref(pipes[7]), N, s);
-  getline(filters, s); std::thread t8(Size_Check, std::ref(pipes[7]), std::ref(pipes[8]), N, std::stoi(s), 1);
-  std::thread t9(FinalPipe, std::ref(pipes[8]), N);
-  filters.close();
-  while(getline(input, s)){
-    Candidate test(s);
-    pipes[0].push(test);
+  if( N != 0){
+    std::vector<Pipe<Candidate>> pipes(9);
+    std::ifstream filters("filters.txt");
+    getline(filters, s); std::thread t1(DateCompare, std::ref(pipes[0]), std::ref(pipes[1]), N, s);
+    getline(filters, s); std::thread t2(EmailAndJob, std::ref(pipes[1]), std::ref(pipes[2]), N, s, 0);
+    getline(filters, s); std::thread t3(EmailAndJob, std::ref(pipes[2]), std::ref(pipes[3]), N, s, 1);
+    getline(filters, s); std::thread t4(SkillCheck, std::ref(pipes[3]), std::ref(pipes[4]), N, s);
+    std::thread t5(CVformat, std::ref(pipes[4]), std::ref(pipes[5]), N);
+    getline(filters, s); std::thread t6(Size_Check, std::ref(pipes[5]), std::ref(pipes[6]), N, std::stoi(s), 0);
+    getline(filters, s); std::thread t7(Picformat, std::ref(pipes[6]), std::ref(pipes[7]), N, s);
+    getline(filters, s); std::thread t8(Size_Check, std::ref(pipes[7]), std::ref(pipes[8]), N, std::stoi(s), 1);
+    filters.close();
+    while(getline(input, s)){
+      Candidate start(s);
+      pipes[0].push(start);
+    }
+    for(int i = 0;i<N;++i){
+      Candidate curr = pipes[8].pop();
+      if(curr.Valid) output << curr._email << std::endl;
+    }
+    t1.join();t2.join();t3.join();t4.join();
+    t5.join();t6.join();t7.join();t8.join();
   }
   input.close();
-  t1.join();t2.join();t3.join();t4.join();
-  t5.join();t6.join();t7.join();t8.join();t9.join();
+  output.close();
 }
