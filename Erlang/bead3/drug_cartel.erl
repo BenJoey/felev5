35fde315a7399fa5b55cbb9c5460ebb1ea89aee2
Bauer Bence <bauerbence5@gmail.com>
2019-01-18 22:08:42 +0100

    Erlang|bead3 done

--- Erlang/bead3/drug_cartel.erl
+++ Erlang/bead3/drug_cartel.erl
@@ -1,11 +1,14 @@
 -module(drug_cartel).
--export([warehouse/1, bad_guy/1]).
+-export([warehouse/1, bad_guy/1, fbi/0]).
 
 warehouse(Password) ->
     register(guard, spawn(fun() -> guard([], Password) end)).
 
 bad_guy(MyPassword) ->
     spawn(fun() -> mafioso(MyPassword) end).
+
+fbi() ->
+    spawn(fun() -> agent() end).
 
 guard(BadGuysIn, PW) ->
     receive
@@ -51,6 +54,27 @@ mafioso(Pw) ->
         run ->
             io:format("I'm running away ~p, the fbi is here.~n", [Pid]);
         {arrest, FbiPid} ->
-            io:format("I surrender"),
+            io:format("I surrender~n"),
             FbiPid ! surrender
     end.
+
+agent() ->
+    Pid = self(),
+    guard ! {'knock_knock', Pid},
+    receive
+        'pw_q' ->
+            guard ! {fbi, Pid}
+    end,
+    receive
+        BadGuys ->
+            timer:sleep(rand:uniform(2000))
+    end,
+    lists:map(fun(L) -> L ! {arrest, self()},
+                        receive
+                            surrender ->
+                                io:format("I arrested ~p~n", [L])
+                        after
+                            3000 ->
+                                io:format("~p run away ~n", [L])
+                        end
+              end, BadGuys).
